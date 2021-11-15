152b20770e0e7be38b792750541ae5dac724b77b
Lucas Manoel <89367719+lucasarruda12@users.noreply.github.com>
2021-11-15 17:03:52 -0300

    dando destino às rotas /, /admin e /funcionario
    o sistema agora renderiza arquivos ejs para cada uma das rotas

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+
 const app = express()
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.json({'msg':'Hello World!'})
+    res.render('index')
 })
 
-app.get('')
+app.get('/admin', (req, res) => {
+    res.render('admin')
+})
+
+app.get('/funcionario', (req, res) =>{
+    res.render('funcionario')
+})
 
 app.listen(8080, () => {
     console.log("Servidor pronto na porta 8080")
