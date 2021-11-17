c33074013552cb30f457a1b9bf185ca5f9e70418
Lucas Manoel <89367719+lucasarruda12@users.noreply.github.com>
2021-11-16 22:12:26 -0300

    Receber, ler e display dados
    Adicionadas as funcionalidades de recebimento de dados através de requisição post na rota /funcionarios e display desses dados na rota /admin.
    
    Ainda não adicionado suporte a banco de dados

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 const express = require('express')
+const port = process.env.PORT || 8080
 
 const app = express()
 app.set('view engine', 'ejs')
+app.use(express.json())
+
+///A SER REMOVIDO:
+const participantes = []
+
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.get('/admin', (req, res) => {
-    res.render('admin')
+    
+    let contador  = 0
+    participantes.forEach((participantes)=>{
+        if(participantes.convidado){
+            contador = contador+2
+        } else{
+            contador = contador+1
+        }
+    })
+
+    res.render('admin', {participantes, contador})
 })
 
 app.get('/funcionario', (req, res) =>{
     res.render('funcionario')
 })
 
-app.listen(8080, () => {
-    console.log("Servidor pronto na porta 8080")
+
+///A SER MODIFICADO:
+app.post('/funcionario', (req, res)=>{
+    participantes.push(req.body)
+    
+    res.json({
+        'status':'success'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Servidor pronto na porta ${port}`)
 })
